d7484a68a5fde399ed4af0834894dd4c73c48d42
Acerkaio <970985882@qq.com>
2024-07-26 15:32:20 +0800

    style upd

--- js/ChangeStyle.js
+++ js/ChangeStyle.js
@@ -1,11 +1,24 @@
 var StyleFlag = 1;
+if (localStorage.getItem("style") == "dark") {
+    StyleFlag = 0;
+    $('#SwitchStyle').html('<ion-icon name="moon-outline"></ion-icon>');
+    $('html').attr('class', 'mdui-theme-dark');
+} else {
+    StyleFlag = 1;
+    $('html').attr('class', 'mdui-theme-light');
+}
 function SwitchStyle() {
     $('html').toggleClass('mdui-theme-dark', 'mdui-theme-light');
     if (StyleFlag == 1) {
         StyleFlag = 0;
         $('#SwitchStyle').html('<ion-icon name="moon-outline"></ion-icon>');
+        localStorage.setItem("style", "dark");
+        localStorage.getItem("style");
+
     } else {
         StyleFlag = 1;
         $('#SwitchStyle').html('<ion-icon name="sunny-outline"></ion-icon>');
+        localStorage.setItem("style", "light");
+        localStorage.getItem("style");
     }
 }
