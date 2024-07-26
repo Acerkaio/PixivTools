f8075777f5374185bc3266d3a78d0511d9722b72
Acerkaio <970985882@qq.com>
2024-07-26 20:51:59 +0800

    search upd

--- js/update.js
+++ js/update.js
@@ -1,12 +1,16 @@
-// 命名规则: 主版本号.功能更新数.bug修复数
-var ver = "1.1.0";
+// 命名规则: 主版本号.功能更新数.bug修复或小更新数
+var ver = "1.1.1";
 var OldVersion = localStorage.getItem("version");
 if (OldVersion != ver) {
     localStorage.setItem("version", ver);
     $("#body").html($("#body").html() + `
         <mdui-dialog close-on-esc class="ver-overlay">
             按下 ESC 关闭
-            <h3>版本: ${ver}</h3>
+            <h2>版本: ${ver}</h2>
+            <h4>[2024.7.26 20:51] 更新内容: </h4>
+            <p>Enter 搜索 (+)</p>
+            <p>原关键词搜索(被代替) (-)</p>
+            <h3>版本 1.10</h3>
             <h4>[2024.7.26 20:08] 更新内容: </h4>
             <p>更新提示 (+)</p>
         </mdui-dialog>
