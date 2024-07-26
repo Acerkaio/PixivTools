7d4be8e1ac6db5bf09e5f91411fe4f63e69fbc18
Acerkaio <970985882@qq.com>
2024-07-27 07:59:56 +0800

    exd upd

--- js/update.js
+++ js/update.js
@@ -1,5 +1,5 @@
 // 命名规则: 主版本号.功能更新数.bug修复或小更新数
-var ver = "1.1.1";
+var ver = "1.1.2";
 var OldVersion = localStorage.getItem("version");
 if (OldVersion != ver) {
     localStorage.setItem("version", ver);
@@ -7,6 +7,10 @@ if (OldVersion != ver) {
         <mdui-dialog close-on-esc class="ver-overlay">
             按下 ESC 关闭
             <h2>版本: ${ver}</h2>
+            <h4>[2024.7.27 07:57] 更新内容: </h4>
+            <p>下载设置了等待，减少内存压力(+)</p>
+            <p>图标增加(+)</p>
+            <h3>版本 1.11</h3>
             <h4>[2024.7.26 20:51] 更新内容: </h4>
             <p>Enter 搜索 (+)</p>
             <p>原关键词搜索(被代替) (-)</p>
